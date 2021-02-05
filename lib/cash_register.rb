19923fc00936b236939111c8c699a3d4102937ff
gusbikos <gusbikos@gmail.com>
2021-02-04 23:08:03 -0500

    commit tree 84b69fca95738db4582145d5e6774d3e46c2031f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister 
 
+    attr_accessor :cash_register_with_discount, :total
+    attr_reader :add_item 
+    
+    def initialize
+        @total = 0 
+        @cash_register_with_discount = 20
+    
+    end
+
+    def add_item(title, price)
+        total = title + price 
+    end 
+
+
+
+
+
+
+
+
+
+
+end
